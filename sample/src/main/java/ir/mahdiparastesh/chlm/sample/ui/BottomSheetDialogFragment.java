40b8939e57dbddf0317a8746b5922392a5c25520
Mahdi Parastesh <fulcrum1378@gmail.com>
2022-02-24 10:14:13 +0330

    2022.02.24 - 1 (MODERNISATION)

--- sample/src/main/java/ir/mahdiparastesh/chlm/sample/ui/BottomSheetDialogFragment.java
+++ sample/src/main/java/ir/mahdiparastesh/chlm/sample/ui/BottomSheetDialogFragment.java
@@ -11,19 +11,15 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 import ir.mahdiparastesh.chlm.ChipsLayoutManager;
 import ir.mahdiparastesh.chlm.SpacingItemDecoration;
 import ir.mahdiparastesh.chlm.sample.R;
+import ir.mahdiparastesh.chlm.sample.databinding.FragmentBottomSheetModalBinding;
 import ir.mahdiparastesh.chlm.sample.entity.ChipsEntity;
 
 public class BottomSheetDialogFragment extends
         com.google.android.material.bottomsheet.BottomSheetDialogFragment {
-
-    @BindView(R.id.rvBottomSheet)
-    RecyclerView rvBottomSheet;
-
+    FragmentBottomSheetModalBinding b;
     private final IItemsFactory<ChipsEntity> itemsFactory = new ChipsFactory();
 
     public static BottomSheetDialogFragment newInstance() {
@@ -35,11 +31,10 @@ public class BottomSheetDialogFragment extends
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_bottom_sheet_modal, container, false);
-        ButterKnife.bind(this, view);
-        return view;
+        b = FragmentBottomSheetModalBinding.inflate(inflater, container, false);
+        return b.getRoot();
     }
 
     private RecyclerView.Adapter<? extends RecyclerView.ViewHolder> createAdapter() {
@@ -51,9 +46,9 @@ public class BottomSheetDialogFragment extends
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         ChipsLayoutManager layoutManager = ChipsLayoutManager.newBuilder(getContext()).build();
-        rvBottomSheet.setLayoutManager(layoutManager);
-        rvBottomSheet.setAdapter(createAdapter());
-        rvBottomSheet.addItemDecoration(new SpacingItemDecoration(
+        b.rvBottomSheet.setLayoutManager(layoutManager);
+        b.rvBottomSheet.setAdapter(createAdapter());
+        b.rvBottomSheet.addItemDecoration(new SpacingItemDecoration(
                 getResources().getDimensionPixelOffset(R.dimen.item_space),
                 getResources().getDimensionPixelOffset(R.dimen.item_space)));
     }
